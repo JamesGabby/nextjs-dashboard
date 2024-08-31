abecf26c1c8483ce656ea1e178aa2e94a125b414
JamesGabby <jlghfe36@gmail.com>
2024-08-31 14:33:44 +0200

    finalise create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,14 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
+  // coerce is changing/casting 'amount' to a number and also typing it as a number
   amount: z.coerce.number(),
   status: z.enum(['pending', 'paid']),
   date: z.string(),
@@ -13,11 +17,19 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
