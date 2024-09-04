8a0e7ab0093b518598835e1d649f13aeb0fc24b1
JamesGabby <jlghfe36@gmail.com>
2024-09-04 18:01:44 +0200

    add metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acevent Dashboard',
+  description: 'The invoice management software.',
+  metadataBase: new URL('https://nextjs-dashboard-khaki-kappa-78.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
